bf87d70c3e9b7053c305a9741577a5e5589e9d54
Kirby Powell <kirbpowell@users.noreply.github.com>
2015-11-08 10:58:00 -0600

    Consolidated chapter 2's answers into a single file.

--- chapter2.js
+++ chapter2.js
@@ -12,3 +12,43 @@ for (var i = 1; i < stop; i++) {
 }
 
 console.log('End problem 1')
+
+// ----------------------------------------------------------------------
+
+var size = 8;
+
+for(var i = 1; i <= 100; ++i) {
+	var output = "";
+
+	if ((i % 3) === 0) {
+		output += "Fizz";
+	}
+
+	if ((i % 5) === 0) {
+		output += "Buzz";
+	}
+
+	if (output === "") {
+		output += i;
+	}
+
+	console.log(output);
+}
+
+// ---------------------------------------------------------------------- 
+
+for (var i = 0; i < size; ++i) {
+	var output = "";
+
+	for (var j = 0; j < size; ++j) {
+
+		if (((i + j) % 2) === 0) {
+			output += " ";
+		}
+		else {
+			output += "#";
+		}
+	}
+
+	console.log(output);
+}
